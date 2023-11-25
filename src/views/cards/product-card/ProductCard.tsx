ceeea64909f1d8afe19bdc25d2a11433bc7d86dd
Majid-Ebrahimi <majidebrahiimi@gmail.com>
2023-11-25 20:37:06 +0330

    redesign

--- src/views/cards/product-card/ProductCard.tsx
+++ src/views/cards/product-card/ProductCard.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image";
 import AspectRatio from "@mui/joy/AspectRatio";
-
 import {
   Box,
-  Button,
   ButtonBase,
-  Card,
   CardContent,
-  CardMedia,
   Grid,
   Typography,
   styled,
@@ -25,9 +21,17 @@ interface Props {
 const ProductButton = styled(ButtonBase)(({ theme }) => ({
   "&:hover": {
     borderRadius: "3px",
-    border: "2px solid var(--primery-color-light-blue-light-blue-10, #D0F3FF)",
+    border: "2px solid #D0F3FF",
     boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.60)",
   },
+}));
+
+const TitleTypography = styled(Typography)(({ theme }) => ({
+  color: "#2C2C34",
+  direction: "rtl",
+  fontSize: "18px",
+  fontWeight: "400",
+  lineHeight: "normal",
 }));
 
 const ProductCard = (props: Props) => {
@@ -42,12 +46,12 @@ const ProductCard = (props: Props) => {
         boxShadow: "none",
         borderRadius: "3px",
         m: 2,
-        p: 2,
+        p: 1,
         pt: 6,
       }}
     >
       <Box>
-        <AspectRatio variant="plain" ratio="6/3" objectFit="contain">
+        <AspectRatio variant="plain" ratio="6/4" objectFit="contain">
           <Image
             loader={() => props.image}
             alt={props.name}
@@ -57,25 +61,59 @@ const ProductCard = (props: Props) => {
           />
         </AspectRatio>
         <CardContent>
-          {props.freeDelivery ? (
-            <Typography>ارسال رایگان</Typography>
-          ) : (
-            <Typography>ارسال سریع</Typography>
-          )}
-          <Typography variant="body1" sx={{ mb: 2 }}>
-            {props.name}
-          </Typography>
-          <Typography variant="body2" sx={{ mb: 2 }}>
-            {props.price}
-          </Typography>
           <Grid display={"flex"}>
-            <Grid item>
-              <Typography sx={{ color: "text.secondary" }}>
-                {props.rating}
-              </Typography>
+            <Grid item sx={{ mx: 0.5 }}>
+              {props.freeDelivery ? (
+                <Image
+                  width={25}
+                  height={25}
+                  alt="star"
+                  src={`delivery-truck.svg`}
+                ></Image>
+              ) : (
+                <Image
+                  width={25}
+                  height={25}
+                  alt="star"
+                  src={`delivery-parcel.svg`}
+                ></Image>
+              )}
             </Grid>
-            <Grid item sx={{ m: 0.3, mx: 0.5 }}>
-              <Image width={18} height={18} alt="star" src={`star.svg`}></Image>
+            <Grid item>
+              {props.freeDelivery ? (
+                <Typography sx={{ mb: 2 }}>ارسال رایگان</Typography>
+              ) : (
+                <Typography sx={{ mb: 2 }}>ارسال فردا</Typography>
+              )}
+            </Grid>
+          </Grid>
+
+          <TitleTypography flexWrap={"wrap"} sx={{ mb: 2 }}>
+            {props.name}
+          </TitleTypography>
+          <Grid display={"flex"} justifyContent={"space-between"}>
+            <Grid display={"flex"}>
+              <Grid item>
+                <Typography variant="body1" sx={{ color: "text.secondary" }}>
+                  {props.rating}
+                </Typography>
+              </Grid>
+              <Grid item sx={{ mx: 0.5 }}>
+                <Image
+                  width={22}
+                  height={22}
+                  alt="star"
+                  src={`star.svg`}
+                ></Image>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" sx={{ mb: 2 }}>
+                ريال{" "}
+                {Intl.NumberFormat("en-US", {
+                  maximumSignificantDigits: 4,
+                }).format(Number(props.price))}
+              </Typography>
             </Grid>
           </Grid>
         </CardContent>
