b7b45bd359ad37a8479ef3c2d11089290c1c8ad1
Majid-Ebrahimi <majidebrahiimi@gmail.com>
2023-12-05 22:16:33 +0330

    some changes

--- src/pages/product-details/index.tsx
+++ src/pages/product-details/index.tsx
@@ -16,6 +16,8 @@ import Image from "next/image";
 import { inherits } from "util";
 import { Container } from "@mui/joy";
 import PriceTypography from "@/components/price-typography";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface CustomSubtitleProps {
   subjectText: string;
@@ -43,7 +45,7 @@ const ProductDetails = () => {
 
   return (
     <Grid container direction="row" justifyContent="center" alignItems="center">
-      {data && (
+      {data ? (
         <>
           <Grid item>
             <Box
@@ -77,7 +79,8 @@ const ProductDetails = () => {
               alignItems="stretch"
               sx={{
                 p: 2,
-                m: 2,
+                my: 3,
+                mx: 6,
                 maxWidth: 500,
                 maxHeight: 500,
                 minHeight: 400,
@@ -124,11 +127,6 @@ const ProductDetails = () => {
                   />
                 </Grid>
               </Grid>
-              {/* <Grid item>
-                <Typography variant="body2">
-                  {data.discountPercentage}%
-                </Typography>
-              </Grid> */}
               <Divider variant="fullWidth" />
               <Grid
                 alignSelf={"end"}
@@ -150,133 +148,28 @@ const ProductDetails = () => {
                     backgroundColor: "#00C0FF",
                     px: 4,
                   }}
+                  onClick={() => {
+                    toast.success(
+                      `با موفقیت به سبد خرید اضافه شد ${data.title}`,
+                      {
+                        position: "top-left",
+                      }
+                    );
+                  }}
                   variant="contained"
                 >
                   افزودن به سبد خرید
                 </Button>
+                <ToastContainer />
               </Grid>
             </Grid>
           </Grid>
         </>
-      )}
-    </Grid>
-  );
-
-  /*  return (
-    <Box>
-      {data ? (
-        <Box
-          // display={"flex"}
-          sx={{
-            p: 2,
-            maxWidth: 600,
-            width: "90%",
-            // flexGrow: 1,
-            backgroundColor: "inherit",
-            // Todo: set DarkMode
-             backgroundColor: (theme) =>
-              theme.palette.mode === "dark" ? "#1A2027" : "#fff", 
-          }}
-        >
-          <Grid
-            container
-            spacing={2}
-            direction="row"
-            display={"flex"}
-            justifyContent={"space-around"}
-          >
-            <Grid display={"inline"}>
-              <Grid
-                style={{
-                  width: "400px",
-                  height: "300px",
-                  position: "relative",
-                }}
-              >
-                <Image
-                  loader={() => data.thumbnail}
-                  alt={data?.title}
-                  src={data?.thumbnail}
-                  fill
-                  unoptimized
-                  sizes="(max-width: 400px) 50vw, 100vw"
-                  style={{
-                    objectFit: "contain",
-                  }}
-                  loading="lazy"
-                />
-              </Grid>
-              {  <Image
-                alt={data.brand}
-                src={data.images[0]}
-                width={50}
-                height={50}
-                unoptimized
-              ></Image>
-              <Image
-                alt={data.brand}
-                src={data.images[1]}
-                width={50}
-                height={50}
-                unoptimized
-              ></Image>
-              <Image
-                alt={data.brand}
-                src={data.images[2]}
-                width={50}
-                height={50}
-                unoptimized
-              ></Image>
-              <Image
-                alt={data.brand}
-                src={data.images[3]}
-                width={50}
-                height={50}
-                unoptimized
-              ></Image> }
-            </Grid>
-            <Grid
-              display={"inline"}
-              container
-              direction="column"
-              spacing={2}
-              sx={{
-                p: 2,
-                border: "1px solid",
-                borderColor: "rgba(0, 0, 0, 0.5)",
-                borderRadius: "3px",
-                boxShadow: "0px 0px 10px #00000080",
-              }}
-            >
-              <Grid>
-                <Typography gutterBottom variant="subtitle1" component="div">
-                  {data.title}
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                  {data.description}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {data.rating}
-                </Typography>
-              </Grid>
-              <Grid>
-                <Typography variant="body2">
-                  {data.discountPercentage}%
-                </Typography>
-              </Grid>
-              <Grid>
-                <Typography variant="subtitle1" component="div">
-                  {data.price * 500000}تومان
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Box>
       ) : (
         <div>somethings wrong!!!</div>
       )}
-    </Box>
-  ); */
+    </Grid>
+  );
 };
 
 export default ProductDetails;
